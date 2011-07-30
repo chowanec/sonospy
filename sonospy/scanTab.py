ecfa3f547fa58be48c38b781c35494c5b86d1c91
John Chowanec <chowanec@gmail.com>
2011-07-30 07:48:33 -0700

    Added multithreading, disabled buttons on scan/repair, defaulted log to disable() until it is used.  Need to figure out how to output STDOUT to LogView in realtime, it still queues all of it and dumps it in at the end...?

--- sonospy/scanTab.py
+++ sonospy/scanTab.py
@@ -4,8 +4,7 @@
 # Copyright, blah, blah
 ###############################################################################
 # TODO:
-#      - Get multithreading to work somehow, so I can process the STDOUT to
-#        LogWindow in realtime.
+#      - Multithreading the STDOUT to LogView.Value in realtime.
 #      - Cosmetic work -- grey out log until it is activated, etc.
 ###############################################################################
 
@@ -13,7 +12,41 @@ import wx
 from wxPython.wx import *
 import os, sys
 import subprocess
-import thread
+from threading import *
+
+# Define notification event for thread completion
+EVT_RESULT_ID = wx.NewId()
+
+def EVT_RESULT(win, func):
+    """Define Result Event."""
+    win.Connect(-1, -1, EVT_RESULT_ID, func)
+
+class ResultEvent(wx.PyEvent):
+    """Simple event to carry arbitrary result data."""
+    def __init__(self, data):
+        """Init Result Event."""
+        wx.PyEvent.__init__(self)
+        self.SetEventType(EVT_RESULT_ID)
+        self.data = data
+
+
+# Worker thread for multi-threading
+class WorkerThread(Thread):
+    """Worker Thread Class."""
+    def __init__(self, notify_window):
+        """Init Worker Thread Class."""
+        Thread.__init__(self)
+        self._notify_window = notify_window
+        self._want_abort = 0
+        self.start()
+
+    def run(self):
+        """Run Worker Thread."""
+        proc = subprocess.Popen([scanCMD], shell=True,stdout=subprocess.PIPE)
+        for line in proc.communicate()[0]:
+            wx.PostEvent(self._notify_window, ResultEvent(line))
+        wx.PostEvent(self._notify_window, ResultEvent(None))
+        return
 
 class ScanPanel(wx.Panel):
     """
@@ -37,11 +70,11 @@ class ScanPanel(wx.Panel):
         sizer.Add(self.tc_MainDatabase, pos=(0, 1), span=(1, 4), flag=wx.TOP|
             wx.EXPAND|wx.ALIGN_CENTER_VERTICAL, border=10)
 
-        bt_MainDatabase = wx.Button(panel, label="Browse...")
-        sizer.Add(bt_MainDatabase, pos=(0, 5), flag=wx.LEFT|wx.RIGHT|wx.TOP|
+        self.bt_MainDatabase = wx.Button(panel, label="Browse...")
+        sizer.Add(self.bt_MainDatabase, pos=(0, 5), flag=wx.LEFT|wx.RIGHT|wx.TOP|
             wx.ALIGN_CENTER_VERTICAL, border=10)
-        bt_MainDatabase.Bind(wx.EVT_BUTTON, self.bt_MainDatabaseClick,
-            bt_MainDatabase)
+        self.bt_MainDatabase.Bind(wx.EVT_BUTTON, self.bt_MainDatabaseClick,
+            self.bt_MainDatabase)
     # --------------------------------------------------------------------------
     # [1] Paths to scan for new Music ------------------------------------------
         self.sb_FoldersToScan = wx.StaticBox(panel, label="Folders to Scan:", size=(200, 100))
@@ -53,31 +86,31 @@ class ScanPanel(wx.Panel):
 
     # --------------------------------------------------------------------------
     # [2] Buttons to Add Folder, Clear Scan Area -------------------------------
-        bt_FoldersToScanAdd = wx.Button(panel, label="Add")
-        bt_FoldersToScanClear = wx.Button(panel, label="Clear")
-        sizer.Add(bt_FoldersToScanAdd, pos=(2,0), span=(1,2), flag=wx.LEFT|wx.RIGHT|
+        self.bt_FoldersToScanAdd = wx.Button(panel, label="Add")
+        self.bt_FoldersToScanClear = wx.Button(panel, label="Clear")
+        sizer.Add(self.bt_FoldersToScanAdd, pos=(2,0), span=(1,2), flag=wx.LEFT|wx.RIGHT|
             wx.ALIGN_CENTER_VERTICAL, border=10)
-        bt_FoldersToScanAdd.Bind(wx.EVT_BUTTON, self.bt_FoldersToScanAddClick, bt_FoldersToScanAdd)
-        sizer.Add(bt_FoldersToScanClear, pos=(2,5), flag=wx.LEFT|wx.RIGHT|
+        self.bt_FoldersToScanAdd.Bind(wx.EVT_BUTTON, self.bt_FoldersToScanAddClick, self.bt_FoldersToScanAdd)
+        sizer.Add(self.bt_FoldersToScanClear, pos=(2,5), flag=wx.LEFT|wx.RIGHT|
             wx.ALIGN_CENTER_VERTICAL, border=10)
-        bt_FoldersToScanClear.Bind(wx.EVT_BUTTON, self.bt_FoldersToScanClearClick, bt_FoldersToScanClear)
+        self.bt_FoldersToScanClear.Bind(wx.EVT_BUTTON, self.bt_FoldersToScanClearClick, self.bt_FoldersToScanClear)
     # --------------------------------------------------------------------------
     # [3] Separator line -------------------------------------------------------
         hl_SepLine1 = wx.StaticLine(panel, 0, (250, 50), (300,1))
         sizer.Add(hl_SepLine1, pos=(3, 0), span=(1, 6), flag=wx.EXPAND, border=10)
     # --------------------------------------------------------------------------
     # [4] Add Scan Options and Scan Button -------------------------------------
-        bt_ScanUpdate = wx.Button(panel, label="Scan/Update")
-        bt_ScanUpdate.Bind(wx.EVT_BUTTON, self.bt_ScanUpdateClick, bt_ScanUpdate)
-        bt_ScanRepair = wx.Button(panel, label="Repair")
-        bt_ScanRepair.Bind(wx.EVT_BUTTON, self.bt_ScanRepairClick, bt_ScanRepair)
+        self.bt_ScanUpdate = wx.Button(panel, label="Scan/Update")
+        self.bt_ScanUpdate.Bind(wx.EVT_BUTTON, self.bt_ScanUpdateClick, self.bt_ScanUpdate)
+        self.bt_ScanRepair = wx.Button(panel, label="Repair")
+        self.bt_ScanRepair.Bind(wx.EVT_BUTTON, self.bt_ScanRepairClick, self.bt_ScanRepair)
         self.ck_ScanVerbose = wx.CheckBox(panel, label="Verbose")
-        bt_SaveLog = wx.Button(panel, label="Save to Log")
-        bt_SaveLog.Bind(wx.EVT_BUTTON, self.bt_SaveLogClick, bt_SaveLog)
-        sizer.Add(bt_ScanUpdate, pos=(4,0), span=(1,2), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
+        self.bt_SaveLog = wx.Button(panel, label="Save to Log")
+        self.bt_SaveLog.Bind(wx.EVT_BUTTON, self.bt_SaveLogClick, self.bt_SaveLog)
+        sizer.Add(self.bt_ScanUpdate, pos=(4,0), span=(1,2), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
         sizer.Add(self.ck_ScanVerbose, pos=(4,2), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
-        sizer.Add(bt_SaveLog, pos=(4,4), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=5)
-        sizer.Add(bt_ScanRepair, pos=(4,5), span=(1,2), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
+        sizer.Add(self.bt_SaveLog, pos=(4,4), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=5)
+        sizer.Add(self.bt_ScanRepair, pos=(4,5), span=(1,2), flag=wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
     # --------------------------------------------------------------------------
     # [5] Separator line ------------------------------------------------------
         hl_SepLine2 = wx.StaticLine(panel, 0, (250, 50), (300,1))
@@ -86,20 +119,43 @@ class ScanPanel(wx.Panel):
     # [6] Output/Log Box -------------------------------------------------------
         self.LogWindow = wx.TextCtrl(panel, -1,"",size=(100, 300), style=wx.TE_MULTILINE|wx.TE_READONLY)
         self.LogWindow.SetInsertionPoint(0)
+        self.LogWindow.Disable()
         sizer.Add(self.LogWindow, pos=(6,0), span=(1,6), flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, border=10)
 
-# DEBUG
-#        self.multiText.Value = "~/Network/Music/Weezer\n"
-#        self.multiText.Value += "~/Network/Music/Yuck"
+# DEBUG ------------------------------------------------------------------------
+        self.multiText.Value = "~/Network/Music/Weezer\n"
+        self.multiText.Value += "~/Network/Music/Yuck"
+#-------------------------------------------------------------------------------
+
+        EVT_RESULT(self,self.OnResult)
+
+        # Indicate we don't have a worker thread yet
+        self.worker = None
 
         sizer.AddGrowableCol(2)
         panel.SetSizer(sizer)
 
-    def bt_ScanRepairClick(self, event):
+    def OnResult(self, event):
+        """Show Result status."""
+        if event.data is None:
+            # Thread aborted (using our convention of None return)
+            self.LogWindow.Value += "\n[Complete]\n"
+            self.setButtons(True)
+            wx.SetCursor(wx.StockCursor(wx.CURSOR_ARROW))
+        else:
+            # Process results here
+            self.LogWindow.Value +=  event.data
+        # In either event, the worker is done
+        self.worker = None
 
+    def bt_ScanRepairClick(self, event):
 # DEBUG
 #        self.tc_MainDatabase.Value = "test.db"
 
+        global scanCMD
+        getOpts = ""
+
+        self.LogWindow.Enable()
         if self.tc_MainDatabase.Value == "":
             self.LogWindow.Value += "ERROR:\tNo database name selected!\n"
         else:
@@ -109,9 +165,8 @@ class ScanPanel(wx.Panel):
             scanCMD = "./scan " + getOpts +"-d " + self.tc_MainDatabase.Value + " -r"
 
             self.LogWindow.Value += "Running Repair on " + self.tc_MainDatabase.Value + "...\n\n"
-            proc = subprocess.Popen([scanCMD],shell=True,stdout=subprocess)
-            for line in proc.communicate()[0]:
-                self.LogWindow.AppendText(line)
+            if not self.worker:
+                self.worker = WorkerThread(self)
 
     def bt_MainDatabaseClick(self, event):
         # Create a list of filters
@@ -143,11 +198,30 @@ class ScanPanel(wx.Panel):
             saveMe.write(self.LogWindow.Value)#get our text from the textctrl, and write it out to the file we just opened.
             saveMe.close()#and then close the file.
 
+    def setButtons(self, state):
+        if state == True:
+            self.bt_FoldersToScanAdd.Enable()
+            self.bt_FoldersToScanClear.Enable()
+            self.bt_MainDatabase.Enable()
+            self.bt_SaveLog.Enable()
+            self.bt_ScanRepair.Enable()
+            self.bt_ScanUpdate.Enable()
+            self.ck_ScanVerbose.Enable()
+        else:
+            self.bt_FoldersToScanAdd.Disable()
+            self.bt_FoldersToScanClear.Disable()
+            self.bt_MainDatabase.Disable()
+            self.bt_SaveLog.Disable()
+            self.bt_ScanRepair.Disable()
+            self.bt_ScanUpdate.Disable()
+            self.ck_ScanVerbose.Disable()
+
     def bt_ScanUpdateClick(self, event):
+        self.LogWindow.Enable()
 
-# DEBUG
-#        self.tc_MainDatabase.Value = "test.db"
-
+# DEBUG ------------------------------------------------------------------------
+        self.tc_MainDatabase.Value = "test.db"
+#-------------------------------------------------------------------------------
         if self.tc_MainDatabase.Value == "":
             self.LogWindow.Value += "ERROR:\tNo database name selected!\n"
         else:
@@ -156,6 +230,7 @@ class ScanPanel(wx.Panel):
             if self.ck_ScanVerbose.Value == True:
                 getOpts = "-v "
 
+            global scanCMD
             scanCMD = "./scan " + getOpts +"-d " + self.tc_MainDatabase.Value + " "
 
             numLines=0
@@ -169,19 +244,9 @@ class ScanPanel(wx.Panel):
                     scanCMD += str(self.multiText.GetLineText(numLines)) + " "
                     numLines += 1
 
-# DEBUG
-                # self.LogWindow.Value += scanCMD
+# Multithreading is below this line.
+                if not self.worker:
+                    self.worker = WorkerThread(self)
+                    self.setButtons(False)
+                    wx.SetCursor(wx.StockCursor(wx.CURSOR_WATCH))
 
-                proc = subprocess.Popen([scanCMD],shell=True,stdout=subprocess.PIPE)
-                for line in proc.communicate()[0]:
-                    self.LogWindow.AppendText(line)
-
-# MULTITHREADING SHIT I CANNOT GET TO WORK!
-
-#                thread.start_new_thread(self.longRunning, (scanCMD,))
-
-#    def longRunning(self, scanCMD):
-#        """This runs in a different thread. """
-#        proc = subprocess.Popen([scanCMD],shell=True,stdout=subprocess.PIPE)
-#        for line in proc.communicate()[0]:
-#            wx.CallAfter(self.LogWindow.AppendText(line))
